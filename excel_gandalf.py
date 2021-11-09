446450e0fd31993d7d40306a90ec20059b183ef7
csizzike <43538455+csizzike@users.noreply.github.com>
2021-11-09 19:51:35 +0100

    minor fixes

--- excel_gandalf.py
+++ excel_gandalf.py
@@ -3,17 +3,7 @@ import pandas as pd
 
 sg.theme('Light Blue 2')
 
-def move(listbox, old_index, new_index, limit):
-    print(str(listbox.get_list_values))
-    if not(0<=old_index<limit and 0<=new_index<limit):
-        return False
-    try:
-        item = listbox.get(old_index)        # Get item by index
-        listbox.delete(old_index)            # Remove item by index
-        listbox.insert(new_index, item)      # insert item by index
-        return True
-    except:
-        return False
+final_list = []
 
 def moveUp(header_list, selected_index):
     if 0 < selected_index <= len(header_list)-1:
@@ -31,32 +21,60 @@ def moveDown(header_list, selected_index):
     if selected_index >= len(header_list):
         return
 
+def copy_and_save_excels(pathfrom, pathto):
+    for file in pathfrom.split(";"):
+        export_path = ''
+        df = pd.read_excel(file)
+        df = df[final_list]
+        file_name = file.split('.')[-2].split('/')[-1]
+        export_path = pathto + "/" + file_name + "_reordered.xlsx"
+        df.to_excel(export_path, index=False)
+
+def open_next_window():
+    layout = [
+        [sg.Text('Apply on', size=(10, 1)),
+         sg.Input(), sg.FileBrowse(button_text='Select')],
+         [sg.Text('Save to', size=(10, 1)),
+         sg.Input(), sg.FolderBrowse(button_text='Browse'),
+         sg.Button("Save reordered excels")]
+    ]
+    window = sg.Window("Second Window", layout, modal=True)
+    choice = None
+    while True:
+        event, values = window.read()
+        if event == "Save reordered excels":
+            copy_and_save_excels(values["Select"], values["Browse"])
+            sg.Popup('The new copies have been saved to: ' + values['Browse'])
+            window.close()
+        if event == "Exit" or event == sg.WIN_CLOSED:
+            break
+        
+    window.close()
+
 layout = [
             [sg.Text('Select your template excel')],
-            [sg.Text('Template excel', size=(8, 1)), sg.Input(), sg.FileBrowse()],
-            [sg.Button("Submit")],
-            [sg.Listbox(values=[], size=(30,6), key="-ITEM-")],
-            [sg.Button("Move item up")],
-            [sg.Button("Move item down")],
-            [sg.Text('Current column order ', size = (50,1), key='-LIST-')]
-          
+            [sg.Text('Template', size=(8, 1)), sg.Input(), sg.FileBrowse(), sg.Button("Load excel")],
+            [sg.Listbox(values=[], size=(30,15), key="-ITEM-", expand_x=True, expand_y=True, highlight_background_color='#6c82b8')],
+            [sg.Button("Move item up"), sg.Button("Move item down")],
+            [sg.Text('Current order ', key='-LIST-', expand_x=True)],
+            [sg.Button("Next")]
           ]
 window = sg.Window('Excel Gandalf', layout, icon='icon\gandalf.ico')
 
 while True:  # Event Loop
     event, values = window.read()
-    #df1 = df1[["Hibá", "Meresi", "Cica", "Kutya"]]
-    #df1.to_excel("test_reordered.xlsx", index=False)
-    if event == 'Submit':
-        df1 = pd.read_excel(values['Browse'])
-        header_list = list(df1)
-        print(header_list)
-        listbox = window['-ITEM-']
-        done = False
-        listbox.Update(values=header_list)
+    if event == 'Load excel':
+        if values['Browse'] != '' and values['Browse'].split('.')[-1] == 'xlsx':
+            df1 = pd.read_excel(values['Browse'])
+            header_list = list(df1)
+            listbox = window['-ITEM-']
+            done = False
+            listbox.Update(values=header_list)
+            window['-LIST-'].update(value="Current order "+repr(header_list))
+        else:
+            sg.Popup('Select an excel file to continue')
 
     elif event == "Move item up" and not done:
-        print("Választott index " + str(listbox.get_indexes()))
         index = listbox.get_indexes()[0]
         moveUp(header_list, index)
         if index == 0:
@@ -67,7 +85,6 @@ while True:  # Event Loop
         window['-LIST-'].update(value="Current order "+repr(final_list))
 
     elif event == "Move item down" and not done:
-        print("Választott index " + str(listbox.get_indexes()))
         index = listbox.get_indexes()[0]
         moveDown(header_list, index)
         if index == len(header_list)-1:
@@ -77,12 +94,10 @@ while True:  # Event Loop
         final_list = list(listbox.get_list_values()) # Get final values in sg.litbox
         window['-LIST-'].update(value="Current order "+repr(final_list))
 
+    elif event == "Next" and not done:
+        open_next_window()
+
     if event == sg.WIN_CLOSED or event == None or event == 'Exit':
-        break
-    if event == 'Show':
-        # Update the "output" text element to be the value of "input" element
-        window['-OUTPUT-'].update(values['-IN-'])
-    if event == ' Cancel':
         break
 
 window.close()
