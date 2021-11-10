a9a65af100db92dc2da1accf4dab46884fdb4b37
csizzike <43538455+csizzike@users.noreply.github.com>
2021-11-10 11:04:19 +0100

    Update excel_gandalf.py

--- excel_gandalf.py
+++ excel_gandalf.py
@@ -2,6 +2,8 @@ import PySimpleGUI as sg
 import pandas as pd
 
 sg.theme('Light Blue 2')
+sg.set_global_icon(icon='icon\gandalf.ico')
+sg.set_options(icon='C:\Máté\ExcelGandalf\icon\gandalf.ico')
 
 final_list = []
 
@@ -33,12 +35,12 @@ def copy_and_save_excels(pathfrom, pathto):
 def open_next_window():
     layout = [
         [sg.Text('Apply on', size=(10, 1)),
-         sg.Input(), sg.FileBrowse(button_text='Select')],
+         sg.Input(), sg.FilesBrowse(button_text='Select', file_types=(('Excel Files', '*.xlsx'),))],
          [sg.Text('Save to', size=(10, 1)),
          sg.Input(), sg.FolderBrowse(button_text='Browse'),
          sg.Button("Save reordered excels")]
     ]
-    window = sg.Window("Second Window", layout, modal=True)
+    window = sg.Window("Apply template on selected excel(s)", layout, modal=True)
     choice = None
     while True:
         event, values = window.read()
@@ -53,28 +55,29 @@ def open_next_window():
 
 layout = [
             [sg.Text('Select your template excel')],
-            [sg.Text('Template', size=(8, 1)), sg.Input(), sg.FileBrowse(), sg.Button("Load excel")],
+            [sg.Text('Template', size=(8, 1)), sg.Input(), sg.FileBrowse(file_types=(('Excel Files', '*.xlsx'),), button_text='Select'), sg.Button("Load excel")],
+            [sg.Text('Excel columns in order', tooltip="Top to bottom equals left to right")],
             [sg.Listbox(values=[], size=(30,15), key="-ITEM-", expand_x=True, expand_y=True, highlight_background_color='#6c82b8')],
             [sg.Button("Move item up"), sg.Button("Move item down")],
-            [sg.Text('Current order ', key='-LIST-', expand_x=True)],
+            [sg.Text('Current order ', size=(100, 10), key='-LIST-', expand_x=True)],
             [sg.Button("Next")]
           ]
-window = sg.Window('Excel Gandalf', layout, icon='icon\gandalf.ico')
+window = sg.Window('Excel Gandalf', layout)
 
 while True:  # Event Loop
     event, values = window.read()
     if event == 'Load excel':
-        if values['Browse'] != '' and values['Browse'].split('.')[-1] == 'xlsx':
-            df1 = pd.read_excel(values['Browse'])
+        if values['Select'] != '' and values['Select'].split('.')[-1] == 'xlsx':
+            df1 = pd.read_excel(values['Select'])
             header_list = list(df1)
+            final_list = header_list
             listbox = window['-ITEM-']
-            done = False
             listbox.Update(values=header_list)
             window['-LIST-'].update(value="Current order "+repr(header_list))
         else:
             sg.Popup('Select an excel file to continue')
 
-    elif event == "Move item up" and not done:
+    elif event == "Move item up":
         index = listbox.get_indexes()[0]
         moveUp(header_list, index)
         if index == 0:
@@ -84,7 +87,7 @@ while True:  # Event Loop
         final_list = list(listbox.get_list_values()) # Get final values in sg.litbox
         window['-LIST-'].update(value="Current order "+repr(final_list))
 
-    elif event == "Move item down" and not done:
+    elif event == "Move item down":
         index = listbox.get_indexes()[0]
         moveDown(header_list, index)
         if index == len(header_list)-1:
@@ -94,7 +97,7 @@ while True:  # Event Loop
         final_list = list(listbox.get_list_values()) # Get final values in sg.litbox
         window['-LIST-'].update(value="Current order "+repr(final_list))
 
-    elif event == "Next" and not done:
+    elif event == "Next":
         open_next_window()
 
     if event == sg.WIN_CLOSED or event == None or event == 'Exit':
